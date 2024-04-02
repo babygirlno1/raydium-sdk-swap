a42d484e3d0ebf93397f5822e0871f5916d7c9c8
Lien <160096951+babygirlno1@users.noreply.github.com>
2024-04-02 14:50:05 +0700

    Update swapConfig.ts

--- src/swapConfig.ts
+++ src/swapConfig.ts
@@ -1,4 +1,17 @@
-export const swapConfig = {
+export interface SwapConfig {
+  executeSwap: boolean;
+  useVersionedTransaction: boolean;
+  tokenAAmount: number;
+  tokenAAddress: string;
+  tokenBAddress: string;
+  maxLamports: number;
+  direction: "in" | "out";
+  liquidityFile: string;
+  maxRetries: number;
+  RPC_URL: string;
+  WALLET_PRIVATE_KEY: string;
+}
+export const swapConfig: SwapConfig = {
   executeSwap: false, // Send tx when true, simulate tx when false
   useVersionedTransaction: true,
   tokenAAmount: 0.01, // Swap 0.01 SOL for USDT in this example
@@ -8,4 +21,6 @@ export const swapConfig = {
   direction: "in" as "in" | "out", // Swap direction: 'in' or 'out'
   liquidityFile: "https://api.raydium.io/v2/sdk/liquidity/mainnet.json",
   maxRetries: 20,
+  RPC_URL: "",
+  WALLET_PRIVATE_KEY: "",
 };
